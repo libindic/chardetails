80ed489b30aab52380af7d135fc33cf4cda9fd73
Jishnu <jishnu7@gmail.com>
2012-08-07 22:11:18 +0530

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Character details Module
+==================
+
+This module helps to get details of a given unicode character.
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+name = "chardetails"
+
+setup(
+    name = name,
+    version = "0.2.1",
+    url = "http://silpa.org.in/chardetails",
+    license = "LGPL-3.0",
+    description = "Unicode character details Library",
+    author = "Santhosh Thottingal",
+    author_email = "santhosh.thottingal@gmail.com",
+    long_description = "This library helps to get details of \
+unicode characters.",
+    packages = find_packages('.'),
+    package_data = {'.'},
+    include_package_data = True,
+    setup_requires = ['setuptools-git'],
+    install_requires = ['setuptools'],
+    zip_safe = False,
+    )
