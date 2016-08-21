6c98e1727beea7c9b6620e1b6336ae206ae03f20
Jerin Philip <monu1618@gmail.com>
2016-08-21 11:42:15 +0530

    WSME REST Capability
    * chardetails/expose.py ->
    Functions which enable WSME REST
    From the result, key 'Characters' has been discarded as a dirty
    hack. This brings uniformity in the type required by signature.
    
    * setup.cfg, setup.py ->
    pbr enabled. Entrypoints added for WSME REST

---
+++ chardetails/expose.py
@@ -0,0 +1,12 @@
+from chardetails.core import CharDetails
+
+def chardetails_getdetails(text):
+    C = CharDetails()
+    result = C.getdetails(text)
+    # Conflict with the signature.
+    del result['Characters']
+    return result
+
+def get_details():
+    return [chardetails_getdetails, str, {str:{str:str}}]
+
